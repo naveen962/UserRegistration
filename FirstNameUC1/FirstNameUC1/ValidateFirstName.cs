bbdcbba5af3c30edb5a6fb79495112fbc72df2c1
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-10 02:48:38 -0700

    program to validate first name is added

---
+++ FirstNameUC1/FirstNameUC1/ValidateFirstName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FirstNameUC1
+{
+    class ValidateFirstName
+    {
+        string result;
+        public string Pattern = "^[A-Z]{1}[a-z]{3}";
+        public bool firstNameCheck(string name)
+        {
+            return Regex.IsMatch(name, Pattern);
+        }
+
+    }
+}
