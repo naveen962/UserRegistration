2f0306169273813add3fec51903f22cdc628fd8c
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-18 21:38:06 -0700

    validate user Registration using Lambda Expression

---
+++ Validate_Using_LambdaExpressionUC13/Validate_Using_LambdaExpressionUC13/Lambda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Validate_Using_LambdaExpressionUC13
+{
+    public class Lambda
+    {
+     
+        public Func<string, string, bool> IsValidPasswordRule = (x, y) => Regex.IsMatch(x, y);
+        public Func<string, string, bool> IsValidFirstName = (x, y) => Regex.IsMatch(x, y);
+        public Func<string, string, bool> IsValidLastName = (x, y) => Regex.IsMatch(x, y);
+        public Func<string, string, bool> IsValidEmail = (x, y) => Regex.IsMatch(x, y);
+        public Func<string, string, bool> IsValidPhoneNo = (x, y) => Regex.IsMatch(x, y);
+
+
+
+    }
+        
+    
+   
+}
+    
+   
