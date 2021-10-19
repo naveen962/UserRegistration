3591b77b26e8a676fefb9f17286ce0bf6e9415ab
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-18 21:34:32 -0700

    usecase 12 is added

---
+++ User_Regex_UC12/User_Regex_UC12/CustomException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace User_Regex_UC12
+{
+    public class CustomException : Exception
+    {
+        /// <summary>
+        /// List of custome errors
+        /// </summary>
+        public enum ExceptionName
+        {
+            Entered_Null,
+            Entered_Empty,
+            Expected_Minimum_length,
+            Failed_To_Follow_Regex
+        }
+
+        ExceptionName exceptionType;
+        /// <summary>
+        /// Constructor to set enum variable type
+        /// </summary>
+        /// <param name="exName"></param>
+        /// <param name="msg"></param>
+        public CustomException(ExceptionName exName, string msg) : base(msg)
+        {
+            this.exceptionType = exName;
+        }
+    }
+}
